bb556447a77dc7992468361430e0b46b6e07d99b
Irvin <irvindominguez1@gmail.com>
2022-04-27 23:36:36 -0600

    Landing page updates

--- js/main.js
+++ js/main.js
@@ -7,38 +7,50 @@
 
 
 function form() {
-  return {
+    return {
 
-    
-    full_name: { errorMessage: "", blurred: false },
-    email: { errorMessage: 'Why', blurred: false },
-    phone: { errorMessage: "", blurred: false },
+        fullName: { errorMessage: "", blurred: false },
+        email: { errorMessage: "", blurred: false },
+        phone: { errorMessage: "", blurred: false },
 
-    blur: function (event) {
-      let ele = event.target;
-      this[ele.name].blurred = true;
-      let rules = JSON.parse(ele.dataset.rules);
-      this[ele.name].errorMessage = this.getErrorMessage(ele.value, rules);
-    },
-    input: function (event) {
-      let ele = event.target;
-      let rules = JSON.parse(ele.dataset.rules);
-      this[ele.name].errorMessage = this.getErrorMessage(ele.value, rules);
-    },
-    submit: function (event) {
-      let inputs = [...this.$el.querySelectorAll("input[data-rules]")];
-      inputs.map((input) => {
-        if (Iodine.is(input.value, JSON.parse(input.dataset.rules)) !== true) {
-          event.preventDefault();
+        blur: function (event) {
+            let ele = event.target;
+            this[ele.name].blurred = true;
+            let rules = JSON.parse(ele.dataset.rules);
+            this[ele.name].errorMessage = this.getErrorMessage(ele.id, ele.value, rules);
+        },
+        input: function (event) {
+            let ele = event.target;
+            let rules = JSON.parse(ele.dataset.rules);
+            this[ele.name].errorMessage = this.getErrorMessage(ele.value, rules);
+        },
+        submit: function (event) {
+            let inputs = [...this.$el.querySelectorAll("input[data-rules]")];
+            inputs.map((input) => {
+                if (Iodine.is(input.value, JSON.parse(input.dataset.rules)) !== true) {
+                    event.preventDefault();
+                }
+            });
+        },
+        getErrorMessage: function (id, value, rules) {
+            let isValid = Iodine.is(value, rules);
+
+            if (isValid !== true) {
+                switch (id) {
+                    case 'fullName': 
+                        return Iodine.setErrorMessage = "Please enter your full name";
+                        break;
+                    case 'email': 
+                        return Iodine.setErrorMessage = "Please enter a valid email address";
+                        break;
+                    case 'phone': 
+                        return Iodine.setErrorMessage = "Please enter a valid phone number";
+                        break;
+                    default: 
+                        return Iodine.getErrorMessage(isValid);
+                }
+            }
+            return "";
         }
-      });
-    },
-    getErrorMessage: function (value, rules) {
-      let isValid = Iodine.is(value, rules);
-      if (isValid !== true) {
-        return Iodine.getErrorMessage(isValid);
-      }
-      return "";
-    }
-  };
+    };
 }
