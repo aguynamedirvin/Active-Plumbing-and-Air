2cfdf914c31ac8a0cd5231eee50238a6bed17eae
Irvin <irvindominguez1@gmail.com>
2022-04-30 04:32:45 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,27 +25,7 @@ function form() {
             inputs.map((input) => {
                 if (Iodine.is(input.value, JSON.parse(input.dataset.rules)) !== true) {
                     event.preventDefault();
-                } else {
-                    console.log('Submitted');
-                    let formElement = document.getElementById("form");
-                    let body = new URLSearchParams(new FormData(formElement)).toString();
-                    return fetch("/", {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                      },
-                      body: body,
-                    })
-                      .then((response) => {
-                        if (response.ok) {
-                          formElement.reset();
-                          alert("Thank you for your message!");
-                        } else {
-                          throw new Error(`Something went wrong: ${response.statusText}`);
-                        }
-                      })
-                      .catch((error) => console.error(error));
-                  }
+                }
             });
         },
         getErrorMessage: function (id, value, rules) {
