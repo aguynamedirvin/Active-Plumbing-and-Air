278ea4cfa4bee7a56a07e11d7760b11272485e96
Irvin <irvindominguez1@gmail.com>
2023-05-10 20:40:06 -0600

    Fixed repeat promotions showing

--- components/promotionComponent.js
+++ components/promotionComponent.js
@@ -1,36 +1,36 @@
-function promotionComponent(promotions) {
+// promotionComponent.js
+function promotionComponent() {
     return {
-        promotions,
+        promotions: [],
         dateString: '',
+        init() {
+            this.getLastDayOfCurrentMonth();
+        },
         getLastDayOfCurrentMonth() {
-            // your existing date calculation code here...
-            return {
-                dateString: '',
-                getLastDayOfCurrentMonth() {
-                    let date = new Date();
-                    let year = date.getFullYear();
-                    let month = date.getMonth() + 1;
-                    let lastDay = new Date(year, month, 0).getDate();
-        
-                    // Array of month names
-                    let monthNames = ["January", "February", "March", "April", "May", "June",
-                    "July", "August", "September", "October", "November", "December"];
-        
-                    // Add ordinal suffix
-                    let suffix;
-                    if (lastDay % 10 == 1 && lastDay != 11) {
-                        suffix = 'st';
-                    } else if (lastDay % 10 == 2 && lastDay != 12) {
-                        suffix = 'nd';
-                    } else if (lastDay % 10 == 3 && lastDay != 13) {
-                        suffix = 'rd';
-                    } else {
-                        suffix = 'th';
-                    }
-                    
-                    this.dateString = `${monthNames[month - 1]} ${lastDay}${suffix}, ${year}`;
-                }
+            let date = new Date();
+            let year = date.getFullYear();
+            let month = date.getMonth() + 1;
+            let lastDay = new Date(year, month, 0).getDate();
+
+            // Array of month names
+            let monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+
+            // Add ordinal suffix
+            let suffix;
+            if (lastDay % 10 == 1 && lastDay != 11) {
+                suffix = 'st';
+            } else if (lastDay % 10 == 2 && lastDay != 12) {
+                suffix = 'nd';
+            } else if (lastDay % 10 == 3 && lastDay != 13) {
+                suffix = 'rd';
+            } else {
+                suffix = 'th';
             }
+            
+            this.dateString = `${monthNames[month - 1]} ${lastDay}${suffix}, ${year}`;
         }
     }
 }
+
+window.promotions = promotionComponent();
